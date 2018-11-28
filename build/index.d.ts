06d41df333750be44dc7791f97262cf2722c568f
Mastermindzh <info@rickvanlieshout.com>
2018-11-28 09:05:35 +0100

    version bump and build

---
+++ build/index.d.ts
@@ -0,0 +1,29 @@
+import * as React from "react";
+import Cookies from "js-cookie";
+
+export interface CookieConsentProps {
+  location?: "top" | "bottom" | "none";
+  style?: object;
+  buttonStyle?: object;
+  contentStyle?: object;
+  children?: React.ReactNode;
+  disableStyles?: boolean;
+  hideOnAccept?: boolean;
+  onAccept?: Function;
+  buttonText?: Function | React.ReactNode;
+  cookieName?: string;
+  cookieValue?: string | boolean | number;
+  debug?: boolean;
+  expires?: number;
+  containerClasses?: string;
+  contentClasses?: string;
+  buttonClasses?: string;
+  buttonId?: string;
+  acceptOnScroll?: boolean;
+  acceptOnScrollPercentage?: number;
+  extraCookieOptions?: object;
+}
+
+export default class CookieConsent extends React.Component<CookieConsentProps, {}> {}
+
+export { Cookies };

1eca7226be47b1dcd520d5c73a5625a05742b6b0
Mihai M <matragunamihai97@gmail.com>
2024-01-25 16:31:37 +0200

    release 0.7.0

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'rct-',
+  corePlugins: {
+    preflight: false,
+  },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

0eddedb65bb8b836c3a0130ea6d76dbbebf2d709
Mastermindzh <info@rickvanlieshout.com>
2026-01-07 12:31:22 +0000

    Deploying to gh-pages from @ Mastermindzh/react-cookie-consent@3d6b52e9d5ed2efc6cb648781d067b225ecc7bab 🚀

---
+++ assets/flippedButtons.story-BBy6nXm9.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{C as n,d as a,I as p}from"./intro-CfoqS5Mt.js";import"./index-CtNveh2g.js";const l={title:"CookieConsent/Flipped Buttons",component:n,parameters:{controls:{expanded:!0}}},t={args:{...a,flipButtons:!0,enableDeclineButton:!0,children:"This website uses cookies. Notice the flipped button order!"},render:i=>e.jsxs(e.Fragment,{children:[e.jsx(p,{}),e.jsx(n,{...i})]})};var o,r,s;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  args: {
+    ...defaultStoryProps,
+    flipButtons: true,
+    enableDeclineButton: true,
+    children: "This website uses cookies. Notice the flipped button order!"
+  } as CookieConsentProps,
+  render: args => <>
+      <Intro />
+      <CookieConsent {...args} />
+    </>
+}`,...(s=(r=t.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};export{t as FlippedButtons,l as default};

59cebbf6e24f22a48ffd7b41af225dab271fedbc
Rick van Lieshout <info@rickvanlieshout.com>
2026-01-21 10:59:41 +0100

    docs: made react globally available for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+// workaround to support older jsxRuntime: classic
+import React from "react";
+globalThis.React = React;
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   controls: {

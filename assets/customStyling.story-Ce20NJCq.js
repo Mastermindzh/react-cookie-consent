cab9d33e2a733cfa1b8db376d0bf88c97a027ca8
Mastermindzh <info@rickvanlieshout.com>
2026-08-12 12:29:42 +0000

    Deploying to gh-pages from @ Mastermindzh/react-cookie-consent@9fe791dbaefc584264fcd4848de6a0b90f3b8e57 🚀

---
+++ assets/customStyling.story-Ce20NJCq.js
@@ -0,0 +1,16 @@
+import{a as e,i as t,n,o as r,r as i,t as a}from"./intro-Du_Tm06d.js";import{n as o,r as s}from"./rolldown-runtime-DkW27tQK.js";var c=s({CustomStyling:()=>u,default:()=>l}),l,u;function d(){return(d=o((()=>{e(),t(),n(),l={title:`CookieConsent/Custom Styling`,component:r,parameters:{controls:{expanded:!0}}},u={args:{...i,style:{background:`red`},buttonStyle:{fontWeight:`bold`},children:`This website uses cookies with custom red styling.`},render:e=>React.createElement(React.Fragment,null,React.createElement(a,null),React.createElement(r,e))},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    ...defaultStoryProps,
+    style: {
+      background: "red"
+    },
+    buttonStyle: {
+      fontWeight: "bold"
+    },
+    children: "This website uses cookies with custom red styling."
+  } as CookieConsentProps,
+  render: args => <>
+      <Intro />
+      <CookieConsent {...args} />
+    </>
+}`,...u.parameters?.docs?.source}}}})))()}export{c as n,d as r,u as t};
